cfd2b47c20c993c956e631ed9f671b61072a651a
hinkleydev <azz@hinkley.dev>
2024-11-15 14:35:13 +0000

    Add DnD seed data plus some more styling

---
+++ server/seedDatadnd.js
@@ -0,0 +1,86 @@
+module.exports = {
+  pages: [
+    {
+      title: 'Currency in Durenbrost',
+      content: `Durenbrõst's currency consists of gold auri, silver elo, and copper rängõn. 
+
+Ten rängõnet are equivalent to one elo, and ten elot to an auri. 
+
+Auri (Gold Pieces)
+The Auri's current design was determined at the beginning of the Third Age - a bright and hopeful post-war time. Its face features the broad-rayed visage of the rising sun, and its rear the shape of a post-Exodia Durenbrõst. 
+
+Auri are made from pure gold, which allows them to hold well their value; from this, the whole currency derives its worth. 
+
+Elo (Silver Pieces)
+The Elo was the highest denomination of coin in Durenbrõst in the first age, at which point it was made from platinum and equal in value to ten myyri (which historians believe was a first-age equivalent to auri). Those who traded elo were, at the time, numbering among Gods and kings.
+
+Nowadays, elo are not valuable; rather, they are worth a tenth of a gold piece (auri). Their value is stable relative to the value of gold - which, in turn, means that it need not have value of its own; instead, the coins are made from a substance known as fool's silver, which is hard and - as the name implies - silver.
+
+The front of the elo features a strange shape of unknown origin, which people speculate to be a pond. As of the last twenty years, the elo's rear features the country's coat of arms.
+
+The fool's silver from which elot are made is sourced from mines in The Rocksplit Coast.
+
+Rängõn (Copper Pieces)
+The Rängõn is the lowliest of coins, and made of near-worthless copper - a metal hardly worth the effort of extracting it from the ground. Rängõnet are believed to have evolved from the earliest currencies in Sadé, which were traded by small communities in a time before the First Age. Of course, the existence of time before the First Age is hotly debated.
+
+Rängõnet are made from copper, which is easily available. These coins are typically undecorated, and counterfeits are more common than minted coins.`,
+      status: 'closed',
+      authorId: 1
+    },
+    {
+      title: 'April Heartbleed',
+      content: `Race: Tiefling 
+
+Class: Fighter
+
+Alignment: Chaotic Neutral
+
+Looks: Key attributes include:
+    • Purple shoulder length wavy hair
+    • Horns which spiral 360 degrees, they are around 7 inches long
+    • Pale skin, with spots of slightly fairer skin around the cheeks, nose and forehead
+    • Blue lips, with a small fang sticking on the left side (from a POV of looking directly at April)
+    • A small dark spot high up on the left cheek (from a POV of looking directly at April)
+    • Gold orbital earrings
+    • Long thin tail, which fluffs out at the end
+
+Traits:
+    • Thorough – Never leaves any stone unturned.
+    • Adventurous – Growing up in the slums, April was key to explore the world further that what her socioeconomic restrictions would allow. This is her motivation for joining the party.
+    • Ideal: Aspiration. I'm determined to make something of myself.
+    • Bond: Those who fight beside me are those worth dying for.
+    • Flaw: (Urchin) Gold seems like a lot of money to me, and I’ll do just about anything for more of it. 
+
+Backstory: Growing up in the slums of Verisil, April never had a lot of opportunity for exploration. She would make few but always close friends, since most of the people in Verisil were not close by. April has always been determined to make something of herself, despite the circumstances of her upbringing, she has always sought to bring prosperity the Heartbleed name. `,
+      status: 'closed',
+      authorId: 2
+    },
+    {
+      title: 'Hope Castillon',
+      content: `Light red colored tiefling with jet black hair and eyes. Her hair is cut short in the back and then grows into a mullet. Her left horn is half covered with an iron-made accessory.
+She has a lip and strange cartillage piercing (left ear). Her body is covered in scars from horns to tail, varying in sites and shapes. 
+She wears a chainmail shirt and chainmail skirt, a light brown sleeveless top over her shirt, white leggings under her skirt, and matching gloves and wide boots.
+She is constantly accompanied by her shield (ignore the fact that it's always a different shape i can't make up my mind), two handaxes at her hips, two daggers hidden on her body, and a flail over her shoulder.`,
+      status: 'closed',
+      authorId: 1
+    },
+    {
+      title: 'Kisu\'o / Kissy',
+      content: `A tabaxi with green eyes and bright-ish pink fur on the shorter side. They wear bright pink and green clothing, with a holy symbol on their chest, and carry a shield and a warhammer—both made of bright (once again) green and pink metal. They're covered in glitter, and leave a trail of it wherever they go.`,
+      status: 'open',
+      authorId: 2
+    }
+  ],
+  users: [
+    { name: 'Cas', email: 'cas@email.com' },
+    { name: 'Azz', email: 'azz@email.com' }
+  ],
+  tags: [
+    {
+      name: 'pc'
+    },
+    {
+      name: 'location'
+    }
+  ]
+}
